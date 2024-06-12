19a122fe66f5da835ce5e80fb8904622f3fce5c6
sc4311 <84731471+sc4311@users.noreply.github.com>
2024-06-12 16:14:41 -0500

    Assignment1 completion

--- hw1-mapper1.py
+++ hw1-mapper1.py
@@ -4,10 +4,10 @@ import sys
 
 for line in reader(sys.stdin):
     try:
-        bur = line[13].strip()
+        boro = line[13].strip()
         crime_type = line[7].strip()
-        if not bur or bur == "BORO_NM" or not crime_type or crime_type == "OFNS_DESC":
+        if not boro or boro == "BORO_NM" or not crime_type or crime_type == "OFNS_DESC":
             continue
-        print(f"{bur}\t{crime_type}")
+        print(f"{boro}\t{crime_type}")
     except IndexError:
         continue
--- hw1-reducer1.py
+++ hw1-reducer1.py
@@ -2,30 +2,30 @@
 import sys
 from collections import defaultdict
 
-bur_crime_count = defaultdict(int)
-bur_crime_types = defaultdict(set)
+boro_crime_count = defaultdict(int)
+boro_crime_types = defaultdict(set)
 
 for line in sys.stdin:
     line = line.strip()
-    bro, crime_type = line.split('\t', 1)
-    bur_crime_count[bro] += 1
-    bur_crime_types[bro].add(crime_type)
+    boro, crime_type = line.split('\t', 1)
+    boro_crime_count[boro] += 1
+    boro_crime_types[boro].add(crime_type)
 
 # Determine the borough with the highest number of crimes
-most_crime_bro = None
+most_crime_boro = None
 most_crime_count = 0
-for boro, count in bur_crime_count.items():
+for boro, count in boro_crime_count.items():
     if count > most_crime_count:
-        most_crime_bro = boro
+        most_crime_boro = boro
         most_crime_count = count
 
 # Print the results
-if most_crime_bro:
-    print(f"Most of the crimes were reported in {most_crime_bro}.")
+if most_crime_boro:
+    print(f"Most of the crimes were reported in {most_crime_boro}.")
     print()
-    print(f"Total number of crimes reported in {most_crime_bro} is {most_crime_count}.")
+    print(f"Total number of crimes reported in {most_crime_boro} is {most_crime_count}.")
     print()
-    print(f"Crime types reported in {most_crime_bro} are: {', '.join(bur_crime_types[most_crime_bro])}.")
+    print(f"Crime types reported in {most_crime_boro} are: {', '.join(boro_crime_types[most_crime_boro])}.")
 
 
 
